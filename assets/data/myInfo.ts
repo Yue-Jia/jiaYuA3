ebcb800e09f92d1904f4adb7d16a3a947d1b0630
Yue-Jia <52579897+Yue-Jia@users.noreply.github.com>
2019-07-27 20:14:13 -0400

    Add files via upload

---
+++ assets/data/myInfo.ts
@@ -0,0 +1,8 @@
+import {jiaYu}from'../../app/jiaYu';
+export    const JIAYU:jiaYu={
+        studentNum:'991469751',
+        name:'Yue Jia',
+        loginname:'jiayu',
+        campus:'Trafagar',
+        assignment:'Assignment 3'
+      }
---
+++ assets/data/mySongs.ts
@@ -0,0 +1,26 @@
+import {song}from'../../app/songInfo';
+export const MYSONGS:song[]=[{
+name:'Composer',
+artist:'Ronghao Li',
+genre:'POP',
+year:'2014',
+pic:'./assets/images/li.jpg'
+},{
+    name:'That Girl in Texas',
+    artist:'Jon Wolfe',
+    genre:'Country',
+    year:'1996',
+    pic:'./assets/images/jon.jpg'
+    },{
+        name:'Long Time No See',
+        artist:'Eason Chan',
+        genre:'POP',
+        year:'2007',
+        pic:'./assets/images/chan.jpg'
+        },{
+            name:'Home',
+            artist:'Michael Buble',
+            genre:'Jazz',
+            year:'2005',
+            pic:'./assets/images/Bu.jpg'
+            }]
